33cae90f8892fb3d809cd2b0f47409a10fc85903
Holboeva <asmoholboyeva@gmail.com>
2025-07-07 17:48:51 +0500

    create category, order delete

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ app = FastAPI()
 async def save_ingredients(session:SessionDep,form:IngredientForm):
     ingredient=await Ingradient.create(session,**dict(form))
     return ingredient
+
+@app.post("/category")
+async def create_category(pk: int, name: str):
+    return {
+        "id": pk,
+        "name": category.name,
+        "message": "Category created"
+    }
+
+@app.delete("/order")
+async def delete_order(pk: int):
+    return {
+        "id": pk,
+        "message": "Order deleted"
+    }
