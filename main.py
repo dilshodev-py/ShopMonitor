0a960e143a07fc59a0c5b706a7208a0153a1bc11
omina <ominabaxramova@gmail.com>
2025-07-07 17:50:33 +0500

    new

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from db.models import Category, Order
+from db.sessions import SessionDep
 
 from db.models import Category, OrderItem
 from db.sessions import SessionDep
@@ -6,6 +10,25 @@ from schemas import CategoryForm, OrderItemForm
 from utils import update
 
 app = FastAPI()
+
+
+@app.post('/order')
+async def create_order(pk : int , session : SessionDep):
+    new_order = Order(**dict(Order))
+    session.add(new_order)
+    await session.commit()
+    await session.refresh(new_order)
+    return {"message" : f"pk = {pk} sonli zakaz qabul qilindi !"}
+
+
+
+@app.post('/category')
+async def create_category(category : CategoryForm , session : SessionDep):
+    new_category = Category(**dict(category))
+    session.add(new_category)
+    await session.commit()
+    await session.refresh(new_category)
+    return {"message" : f" Successful {dict(new_category)}"}
 
 
 @app.patch('/category/{category_id}')
